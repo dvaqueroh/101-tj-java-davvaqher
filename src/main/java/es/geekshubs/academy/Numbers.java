b2d3fc65e6a779def4c5cefd60ea0654cb84bb42
DavidVH <60264680+dvaqueroh@users.noreply.github.com>
2021-08-17 11:50:34 +0200

    Actualizacion David - No funciona bien
    Debo tener un error en la conversion a decimales, y tras mucho tiempo, decido cambiar a otro ejercicio para poder avanzar.

--- src/main/java/es/geekshubs/academy/Numbers.java
+++ src/main/java/es/geekshubs/academy/Numbers.java
@@ -1,19 +1,57 @@
 package es.geekshubs.academy;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Numbers {
+	
+	/* No he conseguido hacerlo funcionar, debe ser un problema con el cambio de entero a double, 
+	 * tras mucho tiempo e intentos cambio a otro ejercicio*/
 
   /**
    * Apply Method
    * @param input
    * @return
    */
+	//ArrayList<Integer> nPositivos;
+	//ArrayList<Integer> nNegativos;
+	//ArrayList<Integer> nZero;
+	int numPos,numNeg,numZero;
+	Double  resPos, resNeg, resZero;
+	
+	
   public List<String> apply(final List<Integer> input) {
-    return null;
-  }
+	  
+	  System.out.println("lista numeros = "+ input.size());
+	  for (int i = 0; i < input.size(); i++) {
+		  int num = input.get(i);
+		  System.out.println("numero: "+ num);
+		  if(num>0) {
+			  //nPositivos.add( num);
+			  numPos+= num;
+		  }
+		  else if (num<0) {
+			  //nNegativos.add( num);
+			  numNeg += num;
+		  }
+		  
+		  else if(num == 0) {
+			  //nZero.add(num);
+			  numZero += num;
+		  }
+		  
+		  // resultados
+		  resPos = (double) (numPos / input.size());
+		  resNeg = (double) (numNeg / input.size());
+		  resZero = (double) (numZero / input.size());
+		  
+	  }
+	  // pasamos los resultados en String
+	  List<String> resultado = Arrays.asList(Double.toString(resPos), Double.toString(resNeg), Double.toString(resZero));
+	  return resultado;
+  }// fin metodo apply
 
-}
+}// fin clase
